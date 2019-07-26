1c205ccba5ea6f7e4cba9b8a1500d16a98b592b9
PriyankDhruv <46101772+PriyankDhruv@users.noreply.github.com>
2019-07-27 01:46:51 +0530

    Add files via upload

---
+++ Assignment - 1/src/java/com/vvp/java/Mutable.java
@@ -0,0 +1,69 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.vvp.java;
+
+import java.io.*;
+import java.net.*;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+/**
+ *
+ * @author PRIYANK DHRUV
+ */
+public class Mutable extends HttpServlet {
+   
+    /** 
+    * Processes requests for both HTTP <code>GET</code> and <code>POST</code> methods.
+    * @param request servlet request
+    * @param response servlet response
+    */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+        int N = Integer.parseInt(request.getParameter("N"));
+        try {
+            for(int i=1; i<=10; i++){
+                
+                out.print(N + " * " + i + " = " + N*i + "<br>" );
+                
+            }
+        } finally { 
+            out.close();
+        }
+    } 
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /** 
+    * Handles the HTTP <code>GET</code> method.
+    * @param request servlet request
+    * @param response servlet response
+    */
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException {
+        processRequest(request, response);
+    } 
+
+    /** 
+    * Handles the HTTP <code>POST</code> method.
+    * @param request servlet request
+    * @param response servlet response
+    */
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /** 
+    * Returns a short description of the servlet.
+    */
+    public String getServletInfo() {
+        return "Short description";
+    }
+    // </editor-fold>
+}
---
+++ Assignment - 1/web/index.jsp
@@ -0,0 +1,19 @@
+<%-- 
+    Document   : index
+    Created on : 27 Feb, 2019, 11:07:42 PM
+    Author     : PRIYANK DHRUV
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
+   "http://www.w3.org/TR/html4/loose.dtd">
+
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>JSP Page</title>
+    </head>
+    <body>
+        <h2>Hello World!</h2>
+    </body>
+</html>
